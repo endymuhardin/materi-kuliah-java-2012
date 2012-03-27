970414eca73e601f0ae474634b4e4dc36cbf2a9e
Endy Muhardin <endy.muhardin@gmail.com>
2012-03-27 19:35:28 +0700

    materi praktikum sesi 1

---
+++ praktikum/sesi-01/belajar-maven/src/main/java/Halo.java
@@ -0,0 +1,9 @@
+/**
+* class untuk mendemokan bahwa aplikasi kita sudah jalan.
+* @author Endy Muhardin
+*/
+public class Halo{
+    public static void main(String[] xx){
+        System.out.println("Halo Icha");
+    }
+}
