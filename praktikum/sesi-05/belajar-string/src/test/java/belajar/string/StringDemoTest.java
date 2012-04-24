44f740de758864eb7b5b274b944f36da5eb5f21a
Endy Muhardin <endy.muhardin@gmail.com>
2012-04-24 20:27:57 +0700

    penjelasan string dan tambahan demo method

--- praktikum/sesi-05/belajar-string/src/test/java/belajar/string/StringDemoTest.java
+++ praktikum/sesi-05/belajar-string/src/test/java/belajar/string/StringDemoTest.java
@@ -4,13 +4,33 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class StringDemoTest{
+
+	@Test
+	public void bandingkanString(){
+	    String nama = "endy";
+
+	    assertTrue(nama.equals("endy"));
+	    assertFalse(nama.equals("dadang"));
+
+	    assertTrue(nama.equalsIgnoreCase("Endy"));
+	    assertFalse(nama.equals("Endy"));
+	}
+
+	@Test
+	public void awalanString(){
+	    String nama = "endy muhardin";
+
+	    assertTrue(nama.startsWith("endy"));
+	    assertFalse(nama.startsWith("dadang"));
+	}
+
 	@Test
 	public void testPanjangString(){
 		StringDemo sd = new StringDemo();
 		String nama = "endy";
 		assertTrue(sd.jumlahHuruf(nama) == 4);
 	}
-	
+
 	@Test
 	public void testPotongString(){
 		StringDemo sd = new StringDemo();
@@ -18,7 +38,7 @@ public class StringDemoTest{
 		assertEquals("y muhar", sd.potong(nama, 3, 10));
 		assertEquals("din", sd.potong(nama, 10));
 	}
-	
+
 	@Test
 	public void testCariString(){
 		StringDemo sd = new StringDemo();
@@ -26,7 +46,7 @@ public class StringDemoTest{
 		assertTrue(sd.ketemu(nama, "endy"));
 		assertFalse(sd.ketemu(nama, "dadang"));
 	}
-	
+
 	@Test
 	public void testGantiString(){
 		StringDemo sd = new StringDemo();
